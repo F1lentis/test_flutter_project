4fa97c0b10bdb3403abdda5849bec0b6ad171b8a
F1Lentis <f1lentis@gmail.com>
2024-03-16 10:52:49 +1000

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Какая-то социальная сеть'),
     );
   }
 }
@@ -38,42 +38,87 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
+    });
+  }
+
+  void _minus() {
+    setState(() {
+      _counter--;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
 
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
 
         title: Text(widget.title),
       ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Клики по плюсу',
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                Padding(padding: EdgeInsets.all(20.5)),
+                Image.asset(
+                    'assets/images/photo.jpg', width: 320, height: 280
+                ),
+                const Text('Леонид Лебедев'),
+                Row(
+                  children: [
+                    Icon(Icons.mail_outline_outlined, size: 25,),
+                    Padding(padding: EdgeInsets.only(left: 2.5)),
+                    const Text('lebedev.la@dvfu.ru'),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: const Text('Кнопкощелкатель'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(1.0),
+                  child: Text(
+                    '$_counter',
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headlineMedium,
+                  ),
+                ),
+                TextButton(onPressed: _incrementCounter, child: const Text('Жмяк')),
+                TextButton(onPressed: _minus, child: const Text('Жмяк в другую сторону')),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 40.5)),
+                const Text('Место работы: ДВФУ'),
+                const Text('Место учебы: ДВФУ'),
+                const Text('Возраст: 33 годика'),
+                const Text('Увлечения: всякие'),
+                Padding(padding: EdgeInsets.only(top: 40.5, bottom: 180.5)),
+
+                CircleAvatar(
+                  backgroundImage: NetworkImage('https://wp-s.ru/wallpapers/5/18/287184122007421/oskalenyj-zelenoglazyj-kot-lezhit-v-trave.jpg'),
+                  radius: 50.5,
+                ),
+                const Text('Хотел еще аватарки таких котанов'),
+                const Text('сделать, чтобы менялись по нажатию'),
+                const Text('кнопки, но чет не получилось'),
+                const Text('...и текст с автопереносом тоже..'),
+                const Text('не получилось))'),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
